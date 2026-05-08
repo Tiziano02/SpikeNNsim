707b2beab3ab16f35b0dd740dd605aa264684a98
Tiziano Costantini <tizianocostantiniw@icloud.com>
2026-05-08 19:50:58 +0200

    introduzione delle unità di misura fondamentali, derivate necessarie. Aggiunta dei multipli e sottomultipli

--- ReteNeurale.hpp
+++ ReteNeurale.hpp
@@ -4,6 +4,43 @@
 #include <string>
 #include <iostream>
 
+
+// Definizione delle unità di misura, dei multipli e sottomultipli
+
+// unità fondamentali del Sistema Internazionale
+const double s =    1.0;                // secondo  
+const double m =    1.0;                // metro
+const double kg =   1.0;                // chilogrammo 
+const double A =    1.0;                // ampere
+
+//unità derivate di nostro utilizzo 
+const double Hz = 1.0/s;                // frequenza
+const double v = m / s;                 // velocità
+const double a = m / (s * s);           // accelerazione
+const double N = kg * m / (s * s);      // forza
+const double rho = kg / (m * m * m);    // densità
+const double Pa = N / (m * m);          // pressione
+const double J = N * m;                 // energia
+const double W = J / s;                 // potenza
+const double C = A * s;                 // carica elettrica
+const double V = J / C;                 // potenziale elettrico
+const double Ohm = V / A;               // resistenza elettrica
+const double S = A / V;                 // conduttanza elettrica
+const double F = C / V;                 // capacità elettrica
+
+
+// multipli e sottomultipli
+const double T = 1e12;               // tera
+const double G = 1e9;                // giga
+const double M = 1e6;                // mega
+const double k = 1e3;                // kilo
+const double m = 1e-3;                // milli
+const double u = 1e-6;                // micro
+const double n = 1e-9;                // nano
+const double p = 1e-12;               // pico
+
+
+
 class Neurone{
 
     private:
@@ -19,10 +56,10 @@ class Neurone{
 
     public:
         // costrutture di default e con parametri per inizializzare i neuroni con i parametri biologici e lo stato dinamico iniziale
-        Neurone(int id) : id_(id), V_(-65.0), Vth_(-50.0), Vrest_(-65.0), Vreset_(-70.0), tau_(10.0), tauR_(5.0), tempoRR_(0.0), fired(false) {}
+        Neurone(int id) : id_(id), V_(-65.0 * m * V), Vth_(-50.0 * m * V), Vrest_(-65.0 * m * V), Vreset_(-70.0 * m * V), tau_(10.0 * m*s), tauR_(5.0 *m* s), tempoRR_(0.0 *s), fired(false) {}
         // costruttore per inizializzare i neuroni con i parametri biologici e lo stato dinamico iniziale
         Neurone(int id, double V_0, double V_th, double V_rest, double V_reset, double tau, double tauR) 
-            : id_(id), V_(V_0), Vth_(V_th), Vrest_(V_rest), Vreset_(V_reset), tau_(tau), tauR_(tauR), tempoRR_(0.0), fired(false) {}
+            : id_(id), V_(V_0), Vth_(V_th), Vrest_(V_rest), Vreset_(V_reset), tau_(tau), tauR_(tauR), tempoRR_(0.0 *s), fired(false) {}
         
 
         // Metodi della classe Neurone    
