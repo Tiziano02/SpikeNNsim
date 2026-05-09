294cd69c312dd7ee3dfbcf25241ba5620c6402e0
Tiziano Costantini <tizianocostantiniw@icloud.com>
2026-05-09 16:34:57 +0200

    correzione oggetto Sinapsi, aggiunta del peso sinaptico e della scala di corrente massima

--- Sinapsi.hpp
+++ Sinapsi.hpp
@@ -6,20 +6,21 @@
 class Sinapsi
 {
 private:
-    double peso_; // forza della sinapsi, positiva per eccitatoria, negativa per inibitoria
+    double peso_; // forza della sinapsi, positiva per eccitatoria, negativa per inibitoria --> [-1,+1] --> in futuro si può fare tipoEccitatoria con A positiva e tipoInibitoria con A negativa e il peso --> [0,1] 
+    double Ipeak_; // modulo corrente generata da uno spike pre-sinaptico per peso unitario 
     int idPre_, idPost_;  // ID del neurone pre-sinaptico e post-sinaptico
-    double Isyn_; // "corrente" sinaptica che contribuisce al potenziale del neurone post-sinaptico
+    double Isyn_; // corrente sinaptica che contribuisce al potenziale del neurone post-sinaptico
     double tau_; // tempo di decadimento della corrente sinaptica
 
 public:
-    Sinapsi(double peso, int idPre, int idPost, double tau) : idPre_(idPre), idPost_(idPost), peso_(peso), Isyn_(0.0) ,tau_(tau) {};
+    Sinapsi(double peso, double Ipeak, int idPre, int idPost, double tau) : peso_(peso), Ipeak_(Ipeak), idPre_(idPre), idPost_(idPost), Isyn_(0.0) ,tau_(tau) {};
     
     // metodo update
     void update(double dt, bool preFired) {
         if (preFired) {
-            Isyn_ += peso_;   
+            Isyn_ += peso_ * Ipeak_;   
         }
-        Isyn_ += dt *  -(Isyn_ /tau_); // decadimento della corrente sinaptica nel tempo
+        Isyn_ += dt *  (-Isyn_ /tau_); // decadimento della corrente sinaptica nel tempo
     }
 
     double getCurrent() const { return Isyn_; }
